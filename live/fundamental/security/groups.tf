2b5eba6eb1ccc96c7ea0e14ac4881709fa444101
Eva Harris <mail@gastrodon.io>
2021-07-13 00:24:20 -0400

    Allow service instance outbound
    Allow service instances to make outbound network requests, this is
    required to at _least_ register with its cluster

--- live/fundamental/security/groups.tf
+++ live/fundamental/security/groups.tf
@@ -8,6 +8,15 @@ resource "aws_security_group" "service" {
   vpc_id = data.terraform_remote_state.network.outputs.vpc_id
 }
 
+resource "aws_security_group_rule" "service_outbound" {
+  to_port           = 0
+  from_port         = 0
+  protocol          = "-1"
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.service.id
+}
+
 resource "aws_security_group_rule" "service_ssh" {
   to_port           = 22
   from_port         = 22
