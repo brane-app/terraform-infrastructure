bd6c493d8ee6e951c08ec48e0757c52d1108e748
Eva Harris <mail@gastrodon.io>
2021-07-12 23:42:51 -0400

    Create security groups
    Create security groups attachable to ecs instances

---
+++ live/fundamental/security/groups.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "database_ingress" {
+  name   = "${var.prefix}-database-ingress"
+  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+}
+
+resource "aws_security_group" "service" {
+  name   = "${var.prefix}-service"
+  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+}
+
+resource "aws_security_group_rule" "service_ssh" {
+  to_port           = 22
+  from_port         = 22
+  protocol          = "TCP"
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.service.id
+}
---
+++ live/fundamental/security/outputs.tf
@@ -0,0 +1,7 @@
+output "group_database_ingress_id" {
+  value = aws_security_group.database_ingress.id
+}
+
+output "group_service_id" {
+  value = aws_security_group.service.id
+}
