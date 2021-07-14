098352e936a76452538971a10a453fa345d91983
Eva Harris <mail@gastrodon.io>
2021-07-14 04:19:42 -0400

    Create the ECS service for self service

--- live/service/self/modules.tf
+++ live/service/self/modules.tf
@@ -13,3 +13,21 @@ module "task_definition" {
   database_password = data.terraform_remote_state.database.outputs.password
   database_endpoint = data.terraform_remote_state.database.outputs.endpoint
 }
+
+module "service" {
+  source = "git::ssh://git@github.com/brane-app/terraform-infrastructure.git//modules/api_service/service?ref=master"
+
+  name   = "self"
+  prefix = var.prefix
+
+  task_definition        = module.task_definition.task_definition_arn
+  listener_paths         = ["/self"]
+  health_check_path      = "/"
+  health_check_matcher   = "401"
+  service_container_port = 8000
+
+  balancer_arn = data.terraform_remote_state.balancer.outputs.balancer_arn
+  listener_arn = data.terraform_remote_state.balancer.outputs.http_listener_arn
+  cluster_arn  = data.terraform_remote_state.cluster.outputs.cluster_arn
+  host_vpc_id  = data.terraform_remote_state.network.outputs.vpc_id
+}
