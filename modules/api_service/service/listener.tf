7a8a2ae248dc68265245c33f5d9f9aad5dbe50a9
Eva Harris <mail@gastrodon.io>
2021-07-14 02:47:23 -0400

    Remove the target group target_type
    I guess we don't need that for ec2 services

--- modules/api_service/service/listener.tf
+++ modules/api_service/service/listener.tf
@@ -2,17 +2,16 @@ resource "aws_lb_target_group" "http" {
   name = "${var.prefix}-${var.name}"
   tags = { "Name" : "${var.prefix}-${var.name}" }
 
-  port        = var.target_group_port
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = var.host_vpc_id
+  port     = var.target_group_port
+  protocol = "HTTP"
+  vpc_id   = var.host_vpc_id
 
   health_check {
     path    = var.health_check_path
     matcher = var.health_check_matcher
   }
 }
-# 
+#
 # resource "aws_lb_listener_rule" "rule" {
 #   listener_arn = var.listener_arn
 #
