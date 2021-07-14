9d5022e3cdf3ba4adf88dbdffab7a8d46db2ccf9
Eva Harris <mail@gastrodon.io>
2021-07-14 02:44:25 -0400

    Don't create a listener rule

--- modules/api_service/service/listener.tf
+++ modules/api_service/service/listener.tf
@@ -12,18 +12,18 @@ resource "aws_lb_target_group" "http" {
     matcher = var.health_check_matcher
   }
 }
-
-resource "aws_lb_listener_rule" "rule" {
-  listener_arn = var.listener_arn
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.http.arn
-  }
-
-  condition {
-    path_pattern {
-      values = var.listener_paths
-    }
-  }
-}
+# 
+# resource "aws_lb_listener_rule" "rule" {
+#   listener_arn = var.listener_arn
+#
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.http.arn
+#   }
+#
+#   condition {
+#     path_pattern {
+#       values = var.listener_paths
+#     }
+#   }
+# }
