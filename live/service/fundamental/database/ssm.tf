0dc9e8f39ee6e3358babc8ebf5403085aaba2308
Eva Harris <mail@gastrodon.io>
2021-07-14 00:17:18 -0400

    Change SSM parameter paths
    Update SSM parameter paths to better reflect where this infrastructure
    is relative to other things, and to not make this seem like it may be a
    service

--- live/service/fundamental/database/ssm.tf
+++ live/service/fundamental/database/ssm.tf
@@ -1,23 +1,23 @@
 resource "aws_ssm_parameter" "username" {
-  name  = "/${var.prefix}/service/database/username"
+  name  = "/${var.prefix}/service/fundamental/database/username"
   value = var.database_username
   type  = "String"
 }
 
 resource "aws_ssm_parameter" "password" {
-  name  = "/${var.prefix}/service/database/password"
+  name  = "/${var.prefix}/service/fundamental/database/password"
   value = var.database_password
   type  = "SecureString"
 }
 
 resource "aws_ssm_parameter" "endpoint" {
-  name  = "/${var.prefix}/service/database/endpoint"
+  name  = "/${var.prefix}/service/fundamental/database/endpoint"
   value = aws_db_instance.database.endpoint
   type  = "String"
 }
 
 resource "aws_ssm_parameter" "DATABASE" {
-  name  = "/${var.prefix}/service/database/DATABASE"
+  name  = "/${var.prefix}/service/fundamental/database/DATABASE"
   value = "services"
   type  = "String"
 }
