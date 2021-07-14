fdfc10f6090ba0ca8742d8cffd07247d2aaf02b6
Eva Harris <mail@gastrodon.io>
2021-07-14 03:55:07 -0400

    Include load balancer security group
    Move the balancer security group into this module, with its rules
    
    Add descriptions to security group rules
    
    Add Name tags to security groups

--- live/fundamental/security/groups.tf
+++ live/fundamental/security/groups.tf
@@ -1,14 +1,27 @@
+resource "aws_security_group" "balancer" {
+  name = "${var.prefix}-services-balancer"
+  tags = { "Name" : "${var.prefix}-services-balancer" }
+
+  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+}
+
 resource "aws_security_group" "database_ingress" {
-  name   = "${var.prefix}-database-ingress"
+  name = "${var.prefix}-database-ingress"
+  tags = { "Name" : "${var.prefix}-database-ingress" }
+
   vpc_id = data.terraform_remote_state.network.outputs.vpc_id
 }
 
 resource "aws_security_group" "service" {
-  name   = "${var.prefix}-service"
+  name = "${var.prefix}-service"
+  tags = { "Name" : "${var.prefix}-service" }
+
   vpc_id = data.terraform_remote_state.network.outputs.vpc_id
 }
 
 resource "aws_security_group_rule" "service_outbound" {
+  description = "Outbound to the web"
+
   to_port           = 0
   from_port         = 0
   protocol          = "-1"
@@ -18,10 +31,47 @@ resource "aws_security_group_rule" "service_outbound" {
 }
 
 resource "aws_security_group_rule" "service_ssh" {
+  description = "Inbound SSH"
+
   to_port           = 22
   from_port         = 22
   protocol          = "TCP"
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.service.id
+}
+
+resource "aws_security_group_rule" "service_ephemeral_inbound" {
+  description = "Outbound to the balancer"
+
+  to_port                  = 65535
+  from_port                = 32768
+  protocol                 = "TCP"
+  type                     = "ingress"
+  source_security_group_id = aws_security_group.balancer.id
+  security_group_id        = aws_security_group.service.id
+}
+
+
+resource "aws_security_group_rule" "balancer_http_inbound" {
+  description = "HTTP inbound"
+
+  to_port           = 80
+  from_port         = 80
+  protocol          = "tcp"
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.balancer.id
+}
+
+
+resource "aws_security_group_rule" "outbond" {
+  description = "Outbound to ECS nodes"
+
+  to_port                  = 65535
+  from_port                = 32768
+  protocol                 = "TCP"
+  type                     = "egress"
+  source_security_group_id = aws_security_group.service.id
+  security_group_id        = aws_security_group.balancer.id
 }
--- live/fundamental/security/outputs.tf
+++ live/fundamental/security/outputs.tf
@@ -1,3 +1,7 @@
+output "group_balancer_id" {
+  value = aws_security_group.balancer.id
+}
+
 output "group_database_ingress_id" {
   value = aws_security_group.database_ingress.id
 }
