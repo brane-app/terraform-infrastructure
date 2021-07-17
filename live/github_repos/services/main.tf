3e9417be26c0b67640a2be48e539af93501cc5bd
Eva Harris <mail@gastrodon.io>
2021-07-17 04:10:55 -0400

    Put for_each above source

--- live/github_repos/services/main.tf
+++ live/github_repos/services/main.tf
@@ -1,7 +1,8 @@
 module "service_repo" {
+  for_each = var.services
+
   source = "git::ssh://git@github.com/brane-app/terraform-infrastructure.git//modules/repo/api_service?ref=master"
 
-  for_each         = var.services
   service_name     = each.value
   repo_description = "The ${each.value} brane microservice"
 
