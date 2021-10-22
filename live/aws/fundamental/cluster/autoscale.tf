2b21f03935c742aee0eeb41afa6c82e33e627bd0
Eva Harris <mail@gastrodon.io>
2021-10-22 12:21:11 -0400

    Use public keys for ssh again

--- live/aws/fundamental/cluster/autoscale.tf
+++ live/aws/fundamental/cluster/autoscale.tf
@@ -47,21 +47,21 @@ resource "aws_iam_instance_profile" "cluster_node" {
   role = aws_iam_role.cluster_node.name
 }
 
-# resource "aws_key_pair" "cluster_node" {
-#   key_name = "${var.prefix}-ecs-service-instance"
-#   tags     = { "Name" : "${var.prefix}-ecs-service-instance" }
-#
-#   public_key = var.public_key
-# }
+resource "aws_key_pair" "cluster_node" {
+  key_name = "${var.prefix}-ecs-service-instance"
+  tags     = { "Name" : "${var.prefix}-ecs-service-instance" }
+
+  public_key = var.public_key
+}
 
 resource "aws_launch_configuration" "cluster_node" {
   name = "${var.prefix}-ecs-service-instance"
 
-  image_id             = jsondecode(data.aws_ssm_parameter.cluster_node_ami.value)["image_id"]
-  iam_instance_profile = aws_iam_instance_profile.cluster_node.name
-  user_data            = data.template_file.node_user_data.rendered
-  instance_type        = "t3.micro"
-  # key_name                    = aws_key_pair.cluster_node.key_name
+  image_id                    = jsondecode(data.aws_ssm_parameter.cluster_node_ami.value)["image_id"]
+  iam_instance_profile        = aws_iam_instance_profile.cluster_node.name
+  user_data                   = data.template_file.node_user_data.rendered
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.cluster_node.key_name
   associate_public_ip_address = false
 
   security_groups = [
