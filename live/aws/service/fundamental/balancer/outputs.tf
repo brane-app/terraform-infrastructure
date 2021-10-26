4a3bc583973e2280fb3aeeed9a255d3fd31951c7
Eva Harris <mail@gastrodon.io>
2021-10-26 16:55:22 -0400

    balancer: output the dns_name and zone_id

--- live/aws/service/fundamental/balancer/outputs.tf
+++ live/aws/service/fundamental/balancer/outputs.tf
@@ -2,6 +2,14 @@ output "balancer_arn" {
   value = aws_lb.balancer.arn
 }
 
+output "balancer_dns_name" {
+  value = aws_lb.balancer.dns_name
+}
+
+output "balancer_zone_id" {
+  value = aws_lb.balancer.zone_id
+}
+
 output "http_listener_arn" {
   value = aws_lb_listener.http.arn
 }
