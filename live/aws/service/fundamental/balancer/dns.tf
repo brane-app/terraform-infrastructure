765067bd8718641ecaa50bca5c61a478821810ac
Eva Harris <mail@gastrodon.io>
2021-10-26 21:07:45 -0400

    Create ferrothorn dns entry
    Add file. and api. prefixes to dns items

--- live/aws/service/fundamental/balancer/dns.tf
+++ live/aws/service/fundamental/balancer/dns.tf
@@ -3,7 +3,20 @@ resource "aws_route53_zone" "zone" {
 }
 
 resource "aws_route53_record" "prefix" {
-  name = "${var.prefix}.${var.domain_name}"
+  name = "api.${var.prefix}.${var.domain_name}"
+
+  zone_id = aws_route53_zone.zone.zone_id
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.balancer.dns_name
+    zone_id                = aws_lb.balancer.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "prefix" {
+  name = "file.${var.prefix}.${var.domain_name}"
 
   zone_id = aws_route53_zone.zone.zone_id
   type    = "A"
