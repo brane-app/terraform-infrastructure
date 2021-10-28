c6b60b0ba6c388bcfbfcfe03436852c666678833
Eva Harris <mail@gastrodon.io>
2021-10-28 18:08:11 -0400

    Create ferrothorn  service deployment

---
+++ live/kubernetes/services/ferrothorn/ferrothorn.tf
@@ -0,0 +1,8 @@
+module "terraform" {
+  # TODO use git repo address
+  source = "/home/zero/Documents/programming/brane/terraform-infrastructure/modules/kubernetes/service/ferrothorn"
+
+  app         = var.app
+  environment = var.environment
+  namespace   = data.terraform_remote_state.namespace.outputs.namespace_name
+}
---
+++ live/kubernetes/services/ferrothorn/variables.tf
@@ -0,0 +1,9 @@
+variable "app" {
+  description = "Name of the app resources are being deployed for"
+  type        = string
+}
+
+variable "environment" {
+  description = "Name of the environment to deploy resources in"
+  type        = string
+}
