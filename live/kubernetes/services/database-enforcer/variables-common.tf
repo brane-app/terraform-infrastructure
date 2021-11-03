1a0f3b18929dcb20f3db14f1609f7efbaf23d7d8
Eva Harris <mail@gastrodon.io>
2021-11-03 02:34:58 -0400

    Don't use variables-common in database-enforcer

--- live/kubernetes/services/database-enforcer/variables-common.tf
+++
@@ -1,9 +0,0 @@
-variable "app" {
-  description = "Application we're deploying resources for"
-  type        = string
-}
-
-variable "environment" {
-  description = "The name of an environment to deploy things into"
-  type        = string
-}
--- live/kubernetes/services/database-enforcer/variables.tf
+++ live/kubernetes/services/database-enforcer/variables.tf
@@ -7,3 +7,13 @@ variable "image_tag" {
   description = "Database Enforcer image tag"
   type        = string
 }
+
+variable "app" {
+  description = "Application we're deploying resources for"
+  type        = string
+}
+
+variable "environment" {
+  description = "The name of an environment to deploy things into"
+  type        = string
+}
