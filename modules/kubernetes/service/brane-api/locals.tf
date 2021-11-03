6a1db15889b3a9a0b65866eec7c0bde1517560b8
Eva Harris <mail@gastrodon.io>
2021-11-03 00:40:23 -0400

    Clean up locals

--- modules/kubernetes/service/brane-api/locals.tf
+++ modules/kubernetes/service/brane-api/locals.tf
@@ -1,11 +1,10 @@
 locals {
   database_connection = "${var.database_user}:${var.database_password}@tcp(${var.database_address}:${var.database_port})/${var.database_name}"
-
-  prefix = "${var.app}-${var.environment}-brane-api"
+  prefix              = "${var.app}-${var.environment}-brane-api"
 
   labels = {
     app         = var.app,
     environment = var.environment,
-    resource    = "${local.prefix}",
+    resource    = local.prefix,
   }
 }
