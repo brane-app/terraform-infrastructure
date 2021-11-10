d4119bc7ab02ab60fe85c0302d38c2c16a7fc0b9
Eva Harris <mail@gastrodon.io>
2021-11-10 17:01:42 -0500

    Create a github state bucket
    Create a state bucket for github resources, and deploy it in a live
    environment

---
+++ live/github/s3/state-bucket/provider.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.38"
+    }
+  }
+
+  backend "s3" {
+    key     = "s3/state-bucket.tfstate"
+    profile = "gas"
+    region  = "us-east-1"
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["050883687565"]
+  region              = "us-east-1"
+  profile             = "gas"
+
+  default_tags {
+    tags = {
+      app         = var.app
+      environment = var.environment
+      source      = "live/github/s3/state-bucket"
+    }
+  }
+}
---
+++ live/github/s3/state-bucket/variables.tf
@@ -0,0 +1,9 @@
+variable "app" {
+  description = "App being deployed"
+  type        = string
+}
+
+variable "environment" {
+  description = "App deployment environment"
+  type        = string
+}
