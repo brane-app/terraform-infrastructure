e5b80dabc0797ef2a07e49f4a6f5deb4abde6729
Eva Harris <mail@gastrodon.io>
2021-11-10 17:35:08 -0500

    Clean up modules
    Get rid of unused modules ( mostly aws stuff )
    
    Put github modules into a parent folder github

--- modules/database-enforcer/task_definition/database-ssm.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  database_connection = "mysql://${var.database_username}:${var.database_password}@${var.database_endpoint}/${var.database_name}"
-}
-
-resource "aws_ssm_parameter" "connection" {
-  name  = "/${var.prefix}/service/${var.name}/DATABASE_CONNECTION"
-  type  = "SecureString"
-  value = local.database_connection
-}
--- modules/ferrothorn/task_definition/database_ssm.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  database_connection = "${var.database_username}:${var.database_password}@tcp(${var.database_endpoint})/${var.database_name}"
-}
-
-resource "aws_ssm_parameter" "connection" {
-  name  = "/${var.prefix}/ferrothorn/FERROTHORN_CONNECTION"
-  type  = "SecureString"
-  value = local.database_connection
-}
