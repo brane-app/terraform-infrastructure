ce91a35d68d95dc30644ebc2f148fcd2300b537b
Eva Harris <mail@gastrodon.io>
2021-11-15 21:17:47 -0500

    Expose api services on port 80

--- modules/kubernetes/service/brane-api/locals.tf
+++ modules/kubernetes/service/brane-api/locals.tf
@@ -2,7 +2,7 @@ locals {
   database_connection = "${var.database_user}:${var.database_password}@tcp(${var.database_address}:${var.database_port})/${var.database_name}"
   prefix              = "${var.app}-${var.environment}-brane-api"
 
-  port = 8000
+  port = 80
 
   labels = {
     app         = var.app,
