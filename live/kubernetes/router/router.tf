871c2d211d46066cad972c17523601505140fd22
Eva Harris <mail@gastrodon.io>
2021-11-16 01:37:57 -0500

    Route to services

--- live/kubernetes/router/router.tf
+++ live/kubernetes/router/router.tf
@@ -10,5 +10,22 @@ resource "kubernetes_ingress" "router" {
       service_name = kubernetes_service.traefik.metadata[0].name
       service_port = local.traefik_port
     }
+
+    dynamic "rule" {
+      for_each = data.terraform_remote_state.services.outputs.routes
+
+      content {
+        http {
+          path {
+            path = rule.value.path
+
+            backend {
+              service_name = rule.value.name
+              service_port = rule.value.port
+            }
+          }
+        }
+      }
+    }
   }
 }
