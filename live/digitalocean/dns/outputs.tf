755f7afd3590b91e5e7661c902db10e617f4de02
Eva Harris <mail@gastrodon.io>
2021-11-17 23:31:33 -0500

    Create output domain_prefix_all
    This should be used by SSL terraform to get certs for all of our domains

--- live/digitalocean/dns/outputs.tf
+++ live/digitalocean/dns/outputs.tf
@@ -2,6 +2,10 @@ output "domain_name" {
   value = var.domain_name
 }
 
+output "domain_prefix_all" {
+  value = var.domain_prefix_api
+}
+
 output "domain_prefix_api" {
   value = var.domain_prefix_api
 }
