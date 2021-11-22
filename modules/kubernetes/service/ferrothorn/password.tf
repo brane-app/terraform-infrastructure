f81bd9e588adaddce1744395573969fe1e2b6eeb
Eva Harris <mail@gastrodon.io>
2021-11-22 13:15:11 -0500

    Don't allow specials in ferrothorn passwords
    Rename random.tf -> password.tf to match the database module

--- modules/kubernetes/service/ferrothorn/password.tf
+++ modules/kubernetes/service/ferrothorn/password.tf
@@ -1,7 +1,9 @@
 resource "random_password" "database" {
-  length = 32
+  length  = 32
+  special = false
 }
 
 resource "random_password" "ferrothorn" {
-  length = 32
+  length  = 32
+  special = false
 }
