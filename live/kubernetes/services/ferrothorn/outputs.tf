881144261d39601f8fe9f87ec2256d4f520ef8da
Eva Harris <mail@gastrodon.io>
2021-11-22 23:50:30 -0500

    Expose ferrothorn_port

--- live/kubernetes/services/ferrothorn/outputs.tf
+++ live/kubernetes/services/ferrothorn/outputs.tf
@@ -2,6 +2,11 @@ output "ferrothorn_address" {
   value = module.ferrothorn.ferrothorn_address
 }
 
+output "ferrothorn_port" {
+  value = module.ferrothorn.ferrothorn_port
+}
+
+
 output "ferrothorn_secret" {
   value     = module.ferrothorn.ferrothorn_secret
   sensitive = true
